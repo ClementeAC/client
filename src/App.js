82b1f546767cbc6be9a0dab4f316346256308b95
Clemente Castejon <clementecastejon3@gmail.com>
2020-12-07 15:40:37 -0400

    Initial commit to Master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './components/Layout/Main'
+import Home from './components/Home';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <BrowserRouter>
+        <Main>
+          <Switch>
+            <Route exact path ="/" component = {Home} />
+            <Route path = "/register" component = {Register}/>
+            <Route path = "/login" component = {Login}/>
+          </Switch>
+          </Main>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
